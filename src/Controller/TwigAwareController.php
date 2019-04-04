c0ed31cb8eb154c78749bd39c6526ea6b89bcd49
Bob den Otter <bob@twokings.nl>
2019-04-04 08:00:10 +0200

    Making "News" work

--- src/Controller/TwigAwareController.php
+++ src/Controller/TwigAwareController.php
@@ -37,7 +37,6 @@ class TwigAwareController extends AbstractController
     {
         $this->snippetManager = $snippetManager;
 
-        $this->snippetManager->registerSnippet('Foo', 'bar');
         $this->snippetManager->registerBoltSnippets();
     }
 
